c20cbed3b591853b90beab6ccc4d2967afd1b5aa
ImSeenOne <cristianramirezdev@hotmail.com>
2024-06-24 12:46:33 -0600

    First commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("asda");;;;
-            const a = 'hola'
+import express from 'express';
+import { PORT } from './config';
+
+const app = express();
+
+app.get('/', (req, res) => {    
+    res.send('Hello World')
+});
+ 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
